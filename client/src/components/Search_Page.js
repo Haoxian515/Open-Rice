e271f58407bb13131bd64f9a2a4a1cc1070cef25
Benson Zheng <benson515@sbcglobal.net>
2019-06-01 14:27:11 -0700

    detail page updated

--- client/src/components/Search_Page.js
+++ client/src/components/Search_Page.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import {connect} from "react-redux";
 
+import image from "../assets/hotpot.jpg";
 
+import NavBar from "./NavBar"
+
+import "./Search_Page.css"
 
 class Search_Page extends Component {
     constructor(props){
@@ -10,13 +14,40 @@ class Search_Page extends Component {
             venue_ids: ""
         }
         console.log("PRINTING SEACH PAGE")
-        console.log(this.state)
+        console.log(this.props)
     }
 
     render(){
+        const detailCard = 
+        <div className="search_page_card"> 
+            <div>
+                <h3>["venue"]["name"]</h3>
+            </div>
+            <div className="search_page_card_preview">
+                <img src={image}></img>
+                <div className="search_page_venue_details">
+                    <p>["location"]["address"]</p>
+                    <p>["categories"][0]["name"]</p>
+                    <p>Description</p>
+                </div>
+            </div>
+            <div>
+                More details expand ^
+            </div>
+        </div>
+
+        // let venue_list = this.props.venue_ids.map( (venue) => 
+        //     <h1>
+        //         {venue}
+        //     </h1>
+        // )
+        const searchResultFor = <div className="search_detail">Search result for " {this.props.searchKey} "</div>
+
         return(
             <div>
-                Search_Page PlaceHolder
+                <NavBar />
+                    {searchResultFor}
+                    {detailCard}
             </div>
         )
     }
@@ -25,8 +56,13 @@ class Search_Page extends Component {
 function mapStateToProps(reduxState){
     // debugger
     return {
+        // venue_ids: reduxState.venue_ids
+        searchKey: reduxState.searchKey,
+        searchLocation: reduxState.searchLocation,
         venue_ids: reduxState.venue_ids
     }
-  }
-  
+}
+
+  //second param pass dispatch functions as second param in connect
+  //dispatch functom from actions creaters 
   export default connect(mapStateToProps, {} )(Search_Page);
