0a48d18ba07584ace4f3b94d917d3a7e19406877
Benson Zheng <benson515@sbcglobal.net>
2019-06-03 19:35:19 -0700

    id passes to search page ready to finish up

--- client/src/components/Search_Page.js
+++ client/src/components/Search_Page.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
 import {connect} from "react-redux";
-
+import axios from "axios";
 import image from "../assets/hotpot.jpg";
 
 import NavBar from "./NavBar"
 
 import "./Search_Page.css"
+
+class Search_Page_Card extends Component{
+    constructor(props){
+        super(props)
+        console.log(this.props.venue_id)
+    }
+
+    getVenueDetails(venue_id){
+        axios.get("http://localhost:3001/api/venue_detail", {   
+            params: {
+                "venue_id": this.props.id
+            }
+        }).then( response => {
+            // this.props.getVenueDetails(response.data)
+            console.log(response.data)
+            let resVenueName = response.data["name"]
+            // venueDetail : bestPhoto : prefix : suffix
+            let resVenuePhoto = response.data["bestPhoto"]["prefix"] + 
+                                "400x400" + 
+                                response.data["bestPhoto"]["suffix"]
+            // this.setState(
+            //     // {venueDetail: response.data}
+            //     {
+            //         venueName: resVenueName,
+            //         venuePhoto: resVenuePhoto
+            //     }
+            // )
+            console.log("SEACH PAGE RH")
+            console.log(this.state)
+        }).catch(err => {
+            console.log("ERROR: " + err)
+        })
+    }
+
+    render(){
+        return (
+            <div className="search_page_card"> 
+                <div>
+                    <h3>["venue"]["name"]</h3>
+                </div>
+                <div className="search_page_card_preview">
+                    <img src={image}></img>
+                    <div className="search_page_venue_details">
+                        <p>["location"]["address"]</p>
+                        <p>["categories"][0]["name"]</p>
+                        <p>Description</p>
+                    </div>
+                </div>
+                <div>
+                    More details expand ^
+                </div>
+            </div>
+        )
+    }
+
+}
 
 class Search_Page extends Component {
     constructor(props){
@@ -17,30 +73,13 @@ class Search_Page extends Component {
         console.log(this.props)
     }
 
-    render(){
-        const detailCard = 
-        <div className="search_page_card"> 
-            <div>
-                <h3>["venue"]["name"]</h3>
-            </div>
-            <div className="search_page_card_preview">
-                <img src={image}></img>
-                <div className="search_page_venue_details">
-                    <p>["location"]["address"]</p>
-                    <p>["categories"][0]["name"]</p>
-                    <p>Description</p>
-                </div>
-            </div>
-            <div>
-                More details expand ^
-            </div>
-        </div>
+    
 
-        // let venue_list = this.props.venue_ids.map( (venue) => 
-        //     <h1>
-        //         {venue}
-        //     </h1>
-        // )
+    render(){
+        const detailCard = this.props.venue_ids.map(venue_id => 
+            <Search_Page_Card venue_id={venue_id} />
+        )
+
         const searchResultFor = <div className="search_detail">Search result for " {this.props.searchKey} "</div>
 
         return(
