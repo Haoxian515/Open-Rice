324cf322c8125a8579d67ef937fd06b35a44c5a2
Benson Zheng <benson515@sbcglobal.net>
2019-07-17 22:04:34 -0700

    cors test

--- server.js
+++ server.js
@@ -34,6 +34,12 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 //CORS
 // app.use(cors);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
